07ee68b780676fd88899068d5d33fb4391cb35d1
Brent Mifsud <brent.mifsud@outlook.com>
2021-02-19 11:53:14 -0500

    Initial Commit

---
+++ Shared/TendrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TendrApp.swift
+//  Shared
+//
+//  Created by Brent Mifsud on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct TendrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
