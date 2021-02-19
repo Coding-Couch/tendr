14bb3b26746203a450b600073beedfbe94702deb
Brent Mifsud <brent.mifsud@outlook.com>
2021-02-19 17:33:48 -0500

    Add HTTPClient and MemeResponse

---
+++ Shared/Networking/Model/Response/MemeResponse.swift
@@ -0,0 +1,15 @@
+//
+//  MemeResponse.swift
+//  Tendr
+//
+//  Created by Brent Mifsud on 2021-02-19.
+//
+
+import Foundation
+
+struct MemeResponse: Identifiable, Codable {
+	var id: String
+	var url: URL
+	var upvotes: Int
+	var downvotes: Int
+}
