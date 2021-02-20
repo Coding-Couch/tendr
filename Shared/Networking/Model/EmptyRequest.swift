65b78f15f315edeeb0a57816d30bb910cc273101
Brent Mifsud <brent.mifsud@outlook.com>
2021-02-19 22:19:00 -0500

    FEATURE(#5) - Add ApiRequest and Endpoint enum

---
+++ Shared/Networking/Model/EmptyRequest.swift
@@ -0,0 +1,10 @@
+//
+//  EmptyRequest.swift
+//  Tendr
+//
+//  Created by Brent Mifsud on 2021-02-19.
+//
+
+import Foundation
+
+struct EmptyRequest: Codable {}
--- Shared/Networking/Model/MemeResponse.swift
+++ Shared/Networking/Model/MemeResponse.swift
@@ -10,6 +10,15 @@ import Foundation
 struct MemeResponse: Identifiable, Codable {
 	var id: String
 	var url: URL
+	var fileType: FileType
 	var upvotes: Int
 	var downvotes: Int
+}
+
+
+enum FileType: String, Codable {
+	case mp4
+	case gif
+	case jpeg
+	case png
 }
