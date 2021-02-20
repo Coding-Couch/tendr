2b995376711951e2f8655518f3a944c0ea72a67c
Brent Mifsud <brent.mifsud@outlook.com>
2021-02-20 01:25:16 -0500

    FEATURE(#5) - Networking fixes and test cases

---
+++ TendrTests (iOS)/JsonLoader.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  TendrTests (iOS)
+//
+//  Created by Brent Mifsud on 2021-02-20.
+//
+
+import Foundation
+
+class JsonLoader {
+	let bundle = Bundle.init(for: JsonLoader.self)
+	
+	func loadJson(fileName: String) throws -> Data {
+		guard let url = bundle.url(forResource: fileName, withExtension: "json") else {
+			throw JsonLoaderError.fileNotFound
+		}
+		
+		return try Data(contentsOf: url)
+	}
+	
+	enum JsonLoaderError: Error {
+		case fileNotFound
+	}
+}
